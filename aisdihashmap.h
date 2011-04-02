d6424fbd8b6bd166620c13c79e0c843ef0b29b8b
wilczek <woolf864@gmail.com>
2011-04-02 18:14:10 +0200

    werja 0.00.2 a do 0.01 jeszcze daleko

--- aisdihashmap.h
+++ aisdihashmap.h
@@ -1,3 +1,4 @@
+// vim: set fdm=marker :
 /**
 @file aisdihashmap.h
 
@@ -19,6 +20,7 @@ ALL RIGHTS RESERVED
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <assert.h>
 
 #define TABSIZE 64000
 
@@ -27,22 +29,23 @@ ALL RIGHTS RESERVED
 /// @returns 0 if keys are equal, non-zero otherwise.
 /// @param key1 First key to compare.
 /// @param key2 Second key to compare.
+//{{{ porownanie
 template <class Key>
 inline int _compFunc(const Key& key1,const Key& key2)
 {
     return (key1==key2);
-};
-
+};//}}}
+//{{{ funkcja hashujaca
 template <class Key>
-inline unsigned hashF(const Key& k){
-	int hash=0;
-	for(int i=0;i<k.length();i++)
-		hash = 37*hash + k[i];
-	hash %= TABSIZE;
-	if(hash < 0)
-		hash += TABSIZE;
-	return hash;
-}
+inline unsigned hashF(const Key& k) {
+    int hash=0;
+    for(int i=0; i<k.length(); i++)
+        hash = 37*hash + k[i];
+    hash %= TABSIZE;
+    if(hash < 0)
+        hash += TABSIZE;
+    return hash;
+}//}}}
 /// A map with a similar interface to std::map.
 template<class K, class V,
          unsigned hashFunc(const K&),
@@ -50,12 +53,14 @@ template<class K, class V,
 class AISDIHashMap
 {
 public:
+//{{{ definicje
     typedef K key_type;
     typedef V value_type;
     typedef std::pair<K,V> data_type;
     typedef unsigned size_type;
-
-    struct ListNode
+//}}}
+//{{{ struct ListNode
+    class ListNode
     {
         typedef std::pair<K,V> T;
         ListNode* next;   ///< WskaŸnik na kolejny element na liœcie/pierœcieniu
@@ -77,22 +82,29 @@ public:
             prev->next = next;
         }
     };
-private:
+//}}}
+//{{{ klasa lista
     class Lista
     {
-        ListNode *node;
+    protected:
+        ListNode *first;
+    public:
         Lista() {
-            node = new ListNode(std::make_pair<K,V>());
+            first = new ListNode(std::make_pair<K,V>());
         }
         Lista(const Lista &org) {
             ListNode *temp;
             node = new ListNode(std::make_pair<K,V>());
-            temp = org.node->next;
-            while(temp != org.node)
+            temp = org.first->next;
+            while(temp != org.first)
             {
-                new ListNode(temp->data,node,node->prev);
+                new ListNode(temp->data,first,first->prev);
                 temp = temp->next;
             }
+        }
+        ~Lista() {
+            clear();
+            delete first;
         }
         ListNode *insert(const data_type &ins) {
             ListNode *temp;
@@ -107,17 +119,146 @@ private:
         }
         ListNode *unsafe_insert(const data_type &ins) {
             new ListNode(ins,node,node->prev);
-
         }
+//{{{ const iterator dla listy
+        class const_iterator : public std::iterator<std::forward_iterator_tag,
+            std::pair<K, V> >
+        {
+        public:
+            typedef std::pair<K, V> T;
+
+        protected:
+            /// Points to the list element
+            ListNode* node;
+            friend class Lista;
+
+            const_iterator(ListNode* x) : node(x) {}
+        public:
+            const_iterator() {}
+            const_iterator(const const_iterator& a) : node(a.node) {}
+
+            inline const T& operator*() const
+            {
+                return node->data;
+            }
+
+            inline const T* operator->() const
+            {
+                return &(node->data);
+            }
+
+            // preincrementacja
+            /*inline*/
+            const_iterator& operator++()
+            {
+                node = node->next;
+                return *this;
+            }
+            // postincrementacja
+            /*inline*/
+            const_iterator operator++(int)
+            {
+                iterator tmp = *this;
+                node = node->next;
+                return tmp;
+            }
+            // predekrementacja
+            /*inline*/
+            const_iterator& operator--()
+            {
+                node = node->prev;
+                return *this;
+            }
+            // postdekrementacja
+            /*inline*/
+            const_iterator operator--(int)
+            {
+                iterator tmp = *this;
+                node = node->prev;
+                return tmp;
+            }
+
+            inline bool operator==(const const_iterator& a) const
+            {
+                return node == a.node;
+            }
+
+            inline bool operator!=(const const_iterator& a) const
+            {
+                return node != a.node;
+            }
+        };
+//}}}
+//{{{ iterator dla listy
+        class iterator : public const_iterator
+        {
+            iterator(ListNode* x) : const_iterator(x) {}
+            friend class Lista;
+
+        public:
+            typedef std::pair<K, V> T;
+            iterator() {}
+            iterator(const const_iterator& a) : const_iterator(a) {}
+            iterator(const iterator& a) {
+                node = a.node;
+            }
+
+            inline T& operator*() const
+            {
+                return node->data;
+            }
+            inline T* operator->() const
+            {
+                return &(node->data);
+            }
+
+            iterator& operator++()
+            {   // preincrementacja
+                //++(*(const_iterator*)this);
+                //return (*this);
+                node = node->next;
+                return *this;
+            }
+
+            iterator operator++(int)
+            {   // postincrementacja
+                //iterator temp = *this;
+                //++*this;
+                //return temp;
+                iterator tmp = *this;
+                node = node->next;
+                return tmp;
+            }
+
+            iterator& operator--()
+            {   // predekrementacja
+                //--(*(const_iterator*)this);
+                //return (*this);
+                node = node->prev;
+                return *this;
+            }
+
+            iterator operator--(int)
+            {   // postdekrementacja
+                //iterator temp = *this;
+                //--*this;
+                //return temp;
+                iterator tmp = *this;
+                node = node->prev;
+                return tmp;
+            }
+        };
+//}}}
+
     };
-
-
+//}}}
 private:
+//{{{ zmienne
     Lista *tab[TABSIZE];
     size_type tabsize;
-
+//}}}
 public:
-
+//{{{ metody tabliy
     AISDIHashMap() {
         int i = 0;
         for(; i<TABSIZE; i++) {
@@ -133,17 +274,31 @@ public:
         }
     }
     ~AISDIHashMap() {
-
+        int i = 0;
+        for(; i<TABSIZE; i++)
+            delete tab[i];
     }
 
     /// Coping constructor.
     explicit AISDIHashMap(const AISDIHashMap<K, V, hashFunc, compFunc>& a);
 
     /// const_iterator.
-    class const_iterator {};
+    //class const_iterator {};
     /// iterator.
-    class iterator {};
+    //class iterator {};
+	
+//}}}
+//{{{ iterator dla tablicy
+    class iterator {
 
+    };
+//}}}
+//{{{ const iterator dla tablicy
+    class const_iterator {
+
+    };
+//}}}
+//{{{ metody tablicy cd.
     /// Returns an iterator addressing the first element in the map.
     inline iterator begin();
     inline const_iterator begin();
@@ -169,21 +324,27 @@ public:
     /// Inserts an element into a map with a specified key value
     /// if one with such a key value does not exist.
     /// @returns Reference to the value component of the element defined by the key.
-    V& operator[](const K& k);
+    V& operator[](const K& k) {
+        size_type hash = hashFunc(k);
+        assert(0);
+        //return (tab[hash][k])->second;
+    }
 
     /// Tests if a map is empty.
-    bool empty( ) const;
+    bool empty( ) const {
+        return (tabsize == 0);
+    }
 
     /// Returns the number of elements in the map.
-    size_type size() const{
-		return tabsize;
-	}
+    size_type size() const {
+        return tabsize;
+    }
 
     /// Returns the number of elements in a map whose key matches a parameter-specified key.
-    size_type count(const K& _Key) const 
-	{
-		assert(0);
-		return 0;
+    size_type count(const K& _Key) const
+    {
+        assert(0);
+        return 0;
     }
 
     /// Removes an element from the map.
@@ -207,4 +368,5 @@ public:
         for(; i<TABSIZE; i++)
             tab[i]->clear();
     }
+//}}}
 };
